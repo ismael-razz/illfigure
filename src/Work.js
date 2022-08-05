22a65f132a256618a6f3cb2f6d11da9dc96baa19
Ismael <razzwebdev@gmail.com>
2022-08-05 16:46:34 -0400

    eighteenth commit

--- src/Work.js
+++ src/Work.js
@@ -6,7 +6,7 @@ export default function Work() {
   return (
     <div id='artwork'>
       <img className='zr1' src={Zani} alt= 'zr1'></img>
-      <div className='mind'>To the Mind of Illfigure</div>
+      <div className='mind'>Mind of Illfigure</div>
       <img className='ir2' src={IR2} alt='IR2'></img>
       <div className='welcome'>WELCOME</div>
 </div>
