6375494b8fdf43c05f88133e164c4069788d48d6
Ismael <razzwebdev@gmail.com>
2022-08-05 20:23:16 -0400

    thirtythird commit

--- src/Footer.js
+++ src/Footer.js
@@ -8,6 +8,7 @@ export default function Footer() {
       <br></br>
       <a href='https://www.instagram.com/ismael.razz/' className='icons'><FaInstagram /></a>
       <a href='https://github.com/ismael-razz' className='icons'><FaGithub /></a>
+      <br></br>
       <a className='privacy' href='https://www.freeprivacypolicy.com/live/3f8e97c7-7ceb-4b13-aa0f-1c73820733be'>Privacy Policy</a>
       <p className='copyright'>© 2022 by Razz Illfigure</p>
       </footer>
