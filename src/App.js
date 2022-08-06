5799b194b82f9f37fc205b3858fb3fca3df1857d
Ismael <razzwebdev@gmail.com>
2022-08-05 20:20:21 -0400

    thirtysecond commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Intro from './Intro'
 import Work from './Work'
 import Web from "./Web"
 import Footer from './Footer'
-import Copyright from './Copyright'
+
 
 
 export default function App() {
