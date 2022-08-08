251d09599968cc0484da4105c78350b083b548dd
Ismael <razzwebdev@gmail.com>
2022-08-08 14:56:39 -0400

    thirty-fifth commit

--- src/Intro.js
+++ src/Intro.js
@@ -4,8 +4,8 @@ export default function Intro() {
   return (
     <div id= "about" className='main-intro'>
       <div className='me'>I'm Izzy</div>
-      <div className='upcoming'>An upcoming <span className='creative'>creative</span> <span className='designer'>designer</span></div>
-      <div className='needs'>From web and UX design to photo and film</div>
+      <div className='upcoming'>A <span className='creative'>creative</span> <span className='designer'>designer</span></div>
+      <div className='needs'>Specializing in UX and Web Design | Dabbling in Photo and Film</div>
     </div>
   )
 }
